e128bf184b797803c9ddad6c9c4eac3a3fe4b3a0
andream16 <andrea.medda.uni@gmail.com>
2018-01-15 11:18:03 +0100

    Fixing build.sh and ignoring .sql files.

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+rm *.sql
 echo "Dumping PSQL . . ."
-sudo -u postgres pg_dump -a -h xx.xxx.xxx.xxx -p xxxx -U postgres -d priceprobe > backup_$(date +"%Y%m%d.%H%M").sql
+sudo -u postgres pg_dump -a -h 54.194.199.230 -p 9042 -U postgres -d priceprobe > backup_$(date +"%Y%m%d").sql
 echo "Dumping Done! Now Removing old docker build . . ."
 docker-compose down --rmi all
 echo "Build Successfully removed! Starting a new one . . ."
-docker-compose up
-echo "Build Successfully Started! Now starting PSQL restore . . ."
-sudo -u postgres psql -h localhost -p 9042 -U postgres -d priceprobe < backup_$(date +"%Y%m%d.%H%M").sql
-echo "PSQL successfully restored!"
+docker-compose up -d
+echo "Build Successfully Started!"
