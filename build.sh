3a83c36a1dc3087ac985c9d1994c8e6891e40aa5
andream16 <andrea.medda.uni@gmail.com>
2018-01-15 11:29:59 +0100

    Adding an echo.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Removing sql file if present . . ."
 rm *.sql
 echo "Dumping PSQL . . ."
-sudo -u postgres pg_dump -a -h 54.194.199.230 -p 9042 -U postgres -d priceprobe > backup_$(date +"%Y%m%d").sql
+sudo -u postgres pg_dump -a -h localhost -p 9042 -U postgres -d priceprobe > backup_$(date +"%Y%m%d").sql
 echo "Dumping Done! Now Removing old docker build . . ."
 docker-compose down --rmi all
 echo "Build Successfully removed! Starting a new one . . ."
