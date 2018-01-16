2817a410f4d258dc13254fc540d7bc5a6d7ac1d5
andream16 <andrea.medda.uni@gmail.com>
2018-01-16 15:57:44 +0100

    Working version for FE

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm *.sql
 echo "Dumping PSQL . . ."
 sudo -u postgres pg_dump -a -h localhost -p 9042 -U postgres -d priceprobe > backup_$(date +"%Y%m%d").sql
 echo "Dumping Done! Now Removing old docker build . . ."
-docker-compose down --rmi all
+sudo docker-compose down --rmi all
 echo "Build Successfully removed! Starting a new one . . ."
-docker-compose up -d
+sudo docker-compose up -d
 echo "Build Successfully Started!"
